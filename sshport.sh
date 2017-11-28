e1e9e217a99a439abc3c604665c62229cf37b4ee
wangwei8828 <wangwei8828@gmail.com>
2017-11-28 16:38:47 +0800

    Update sshport.sh

--- sshport.sh
+++ sshport.sh
@@ -27,5 +27,6 @@ echo "Port ${sshport}" >> /etc/ssh/sshd_config
 
 iptables -A INPUT -p tcp -m tcp --dport $sshport -j ACCEPT
 
-/etc/init.d/iptables restart 
-/etc/init.d/sshd restart 
+systemctl restart iptables 
+systemctl restart sshd
+echo "done."
